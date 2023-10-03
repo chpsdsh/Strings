d7fde1b7a133cd4d02404a01d2b9f8ae2779fb3c
chpsdsh <144603342+chpsdsh@users.noreply.github.com>
2023-10-03 14:06:17 +0700

    2.c

--- 2.c
+++ 2.c
@@ -6,13 +6,14 @@ int main(){
     int CntSym = 1, MaxProb = 0;
     gets(str);
     for(int i = 0; i < strlen(str) - 1; i++) {
-        if (str[i] == str[i+1])
+        if (str[i] == str[i+1]){
             CntSym++;
-        else {
             if (MaxProb < CntSym)
                 MaxProb = CntSym;
-            CntSym = 1;
         }
+        else
+            CntSym = 1;
+
     }
     printf("%d", MaxProb);
     return 0;
