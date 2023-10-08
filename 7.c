97a01f765fa636411e1292a7f8138a1dbffd91d7
chpsdsh <144603342+chpsdsh@users.noreply.github.com>
2023-10-08 15:41:04 +0700

    Add files via upload

---
+++ 7.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+#include <malloc.h>
+
+
+void min(int *arr,int n){
+    int mn = 9999999;
+    for(int i = 0; i < n; i++)
+        if (mn>*(arr+i))
+            mn = *(arr+i);
+    printf("%d",mn);
+}
+
+
+void razvorot(char *str){
+    int nac = 0, kon,n = 0;
+    int *dliny =malloc(sizeof(int));;
+    for(int i = 0; i < strlen(str); i++){
+        if(str[i] == ' ') {
+            kon = i;
+            dliny[n]=kon-nac-1;
+            nac = i;
+            n++;
+            realloc(dliny,n*sizeof(int));
+        }
+    }
+    min(dliny,n);
+    free(dliny);
+}
+
+
+int main(){
+    char str1[256], str2[256];
+    gets(str1);
+    strcat(str2,str1);
+    str2[strlen(str2)] = ' ';
+    str2[strlen(str2)+1] = '\0';
+    razvorot(str2);
+    return 0;
+}
